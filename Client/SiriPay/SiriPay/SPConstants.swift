ee3be81db94e9387cca860afd63b8391777325f2
Himadri Jyoti <himadri@phonepe.com>
2016-09-10 19:37:45 +0530

    Payments controller complete

---
+++ Client/SiriPay/SiriPay/SPConstants.swift
@@ -0,0 +1,56 @@
+//
+//  SPConstants.swift
+//  SiriPay
+//
+//  Created by Himadri Sekhar Jyoti on 10/09/16.
+//  Copyright © 2016 PhonePe Internet Private Limited. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+let SignInId = "wxxe0ka6yw-signin"
+let SignInSecretKey = "86f126179071da8308e2144a3dbb6fb7"
+
+let SubscriptionId = "test-signup"
+let SubscriptionSecretKey = "c78ec84e389814a05d3ae46546d16d2e"
+
+/*
+ set Vanity/bill/return url
+ */
+let VanityUrl = "nativeSDK"
+let LoadWalletReturnUrl = "https://salty-plateau-1529.herokuapp.com/redirectURL.sandbox.php"
+let BillUrl = "https://salty-plateau-1529.herokuapp.com/billGenerator.sandbox.php"
+
+
+////Test
+// TestParams.h should be populated according to your needs
+//let TEST_EMAIL = "mukeshcitrus122@mailinator.com"
+let TEST_EMAIL = "himadri@gmail.com"
+let TEST_PASSWORD = "tester@123"
+//let TEST_MOBILE = "9700000000"
+let TEST_MOBILE = "9986800922"
+
+let TEST_FIRST_NAME = "test"
+let TEST_LAST_NAME = "user"
+
+let TEST_CITY = "Pune"
+let TEST_COUNTRY = "India"
+let TEST_STATE = "Maharashtra"
+let TEST_STREET1 = "Baner Road"
+let TEST_STREET2 = "Cool City"
+let TEST_ZIP = "411045"
+
+let TEST_CREDIT_CARD_NUMBER = "4111111111111111"
+let TEST_CREDIT_CARD_EXPIRY_DATE = "12/99"
+let TEST_CREDIT_CARD_SCHEME = "visa"
+let TEST_CREDIT_CARD_OWNER_NAME = "Mukesh patil"
+let TEST_CREDIT_CARD_BANK_NAME = "ICICI (credit card)"
+let TEST_CREDIT_CARD_CVV = "018"
+
+let TEST_NETBAK_CODE = "CID001"
+let TEST_NETBAK_NAME = "ICICI Bank"
+
+let TEST_TOKENIZED_CARD_TOKEN = "d832babbbef60bd21b4c12a6cdb726cb"
+let TEST_TOKENIZED_CARD_CVV = "000"
--- Client/SiriPay/SiriPay/SPPaymentController.swift
+++ Client/SiriPay/SiriPay/SPPaymentController.swift
@@ -16,5 +16,67 @@ class SPPaymentController {
     var contactInfo : CTSContactUpdate?
     var addressInfo : CTSUserAddress?
     var customParams: [String : AnyObject] = [:]
+    
+    static let sharedInstance: SPPaymentController = SPPaymentController()
+    
+    
+    func initializeSDK() -> Void {
+        
+        let keyStore : CTSKeyStore = CTSKeyStore()
+        keyStore.signinId = SignInId
+        keyStore.signinSecret = SignInSecretKey
+        keyStore.signUpId = SubscriptionId
+        keyStore.signUpSecret = SubscriptionSecretKey
+        keyStore.vanity = VanityUrl
+        CitrusPaymentSDK.initializeWithKeyStore(keyStore, environment: CTSEnvProduction)
+        
+        //        #if PRODUCTION_MODE
+        //            CitrusPaymentSDK.initializeWithKeyStore(keyStore, environment: CTSEnvProduction)
+        //            #else
+        //            CitrusPaymentSDK.initializeWithKeyStore(keyStore, environment: CTSEnvSandbox)
+        //        #endif
+        
+        
+        CitrusPaymentSDK.enableDEBUGLogs()
+        
+        CitrusPaymentSDK.enableLoader()
+        
+        CitrusPaymentSDK.setLoaderColor(UIColor .orangeColor())
+        
+        self.authLayer = CTSAuthLayer.fetchSharedAuthLayer()
+        self.profileLayer = CTSProfileLayer.fetchSharedProfileLayer()
+        self.paymentLayer = CTSPaymentLayer.fetchSharedPaymentLayer()
+        
+        
+        contactInfo?.firstName = TEST_FIRST_NAME;
+        contactInfo?.lastName = TEST_LAST_NAME;
+        contactInfo?.email = TEST_EMAIL;
+        contactInfo?.mobile = TEST_MOBILE;
+        
+        addressInfo?.city = TEST_CITY;
+        addressInfo?.country = TEST_COUNTRY;
+        addressInfo?.state = TEST_STATE;
+        addressInfo?.street1 = TEST_STREET1;
+        addressInfo?.street2 = TEST_STREET2;
+        addressInfo?.zip = TEST_ZIP;
+        
+        customParams = ["USERDATA2":"MOB_RC|9988776655",
+                        "USERDATA10":"test",
+                        "USERDATA4":"MOB_RC|test@gmail.com",
+                        "USERDATA3":"MOB_RC|4111XXXXXXXX1111"]
+        
+    }
+    
+    
+    func requestOTPForSignIn(email emailString: String, mobileNo mobileNoString: String, completionHandler:ASMasterLinkCallback)  {
+        self.authLayer?.requestMasterLink(emailString,
+                                          mobile: mobileNoString,
+                                          scope:CTSWalletScopeFull,
+                                          completionHandler: completionHandler)
+    }
+    
+    func doSignIn(otp otpString: String, completionHandler: ASCitrusSigninCallBack) {
+        self.authLayer?.requestMasterLinkSignInWithPassword(otpString, passwordType: PasswordTypeOtp, completionHandler: completionHandler)
+    }
 
 }
